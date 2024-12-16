1f151406011e8b4e68fd00fe24887ab3db882b9f
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-16 05:21:12 +0200

    Redesign home page layout and content.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,52 @@
 </head>
 <body class="font-inter">
   <x-navbar />
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="bg-white">
+    <main>
+      <div class="relative isolate">
+        <div class="overflow-hidden">
+          <div class="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+            <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+              <div class="relative w-full lg:max-w-xl lg:shrink-0 xl:max-w-2xl">
+                <h1 class="text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Enkel catering til arbeidsplassen – Bestill i dag!</h1>
+                <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:max-w-md sm:text-xl/8 lg:max-w-none">Vi leverer smakfulle lunsjer, møtemat og overtidsmåltider direkte til døren. Bestill før kl. 14.00 for levering neste dag. Velg blant fleksible betalingsløsninger: Vipps, faktura eller kort. Alt leveres praktisk singelpakket.</p>
+                <div class="mt-10 flex items-center gap-x-6">
+                  <a href="{{ route('items.index') }}" class="rounded-md bg-gray-900 px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Se menyen og bestill</a>
+                </div>
+              </div>
+              <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                  <div class="relative">
+                    <img src="{{ asset('images/cta/shapeimage_1.png') }}" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
+                    <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
+                  </div>
+                </div>
+                <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                  <div class="relative">
+                    <img src="{{ asset('images/cta/shapeimage_2.png') }}" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
+                    <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
+                  </div>
+                  <div class="relative">
+                    <img src="{{ asset('images/cta/shapeimage_3.png') }}" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
+                    <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
+                  </div>
+                </div>
+                <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                  <div class="relative">
+                    <img src="{{ asset('images/cta/shapeimage_4.png') }}" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
+                    <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
+                  </div>
+                  <div class="relative">
+                    <img src="{{ asset('images/cta/shapeimage_5.png') }}" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
+                    <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </body>
 </html>
