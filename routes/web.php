a364355ebc47981e27072af332d643994b057d24
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-16 02:30:52 +0200

    Define routes for login and registration actions (GET/POST).

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/', function() {
     return view('index');
 });
+
+Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthenticationController::class, 'login'])->name('login.process');
+
+Route::get('/register', [AuthenticationController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthenticationController::class, 'register'])->name('register.process');
