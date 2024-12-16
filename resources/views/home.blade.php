0ad29bd7687433579641efee3baf93d6127e9940
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-16 04:49:41 +0200

    Create new homepage view.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="font-inter">
+  <x-navbar />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
