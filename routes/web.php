2811f286dfbb9719e0a7c3c94bcef1e47f3a9615
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-16 14:30:57 +0200

    Define "/dashboard" route protected by access-dashboard gate.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\ItemsController;
 
+use Illuminate\Support\Facades\Gate;
+
 Route::get('/', function() {
     return redirect()->route('home');
 });
@@ -11,6 +13,14 @@ Route::get('/', function() {
 Route::get('/home', function() {
     return view('home');
 })->name('home');
+
+Route::get('/dashboard', function() {
+    if (!Gate::allows('access-dashboard')) {
+        abort(403);
+    }
+
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
 
 Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'login'])->name('login.process');
