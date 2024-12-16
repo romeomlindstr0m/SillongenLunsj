43f0494b3f1f49feba6c5b23236d1df8288f6388
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-16 03:53:18 +0200

    Include item-input.js in static assets.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/authentication-failure.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/authentication-failure.js', 'resources/js/item-input.js'],
             refresh: true,
         }),
     ],
