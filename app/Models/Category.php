25ed2bb50a9ce510ae422ff1188082a3b75a6e20
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-16 03:53:33 +0200

    Create Category model to fetch all item categories.

---
+++ app/Models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
+}
