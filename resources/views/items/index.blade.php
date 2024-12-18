df4484489d92e4bf3af71ef5cbc502d8008baf6c
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-18 10:18:04 +0200

    Add POST form with order information input fields to items/index.blade.php.

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -11,6 +11,7 @@
     <x-navbar />
     <div class="bg-white">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <form action="#" method="POST">
             @foreach ($grouped_items as $category_id => $items)
                 <h2 class="text-3xl font-bold tracking-tight text-gray-900 mb-4">{{ $items->first()->category->name }}</h2>
                 <div class="relative mb-12">
@@ -40,10 +41,77 @@
         </div>
       </div>
 
+      <div class="relative isolate bg-white">
+        <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+          <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
+            <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+              <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
+              </div>
+              <h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Betingselser / levering</h2>
+              <ul class="mt-6 text-lg/8 text-gray-600">
+                <li>Bestilling innen kl.12.00 dagen i forveien for levering.</li>
+                <li>Priser:
+                    <ul class="list-disc">
+                        <li>Hverdager 08.00 - 17.00: kr. 115,- (avtalekunde)</li>
+                        <li>Hverdager etter 17.00: kr. 159,-</li>
+                        <li>Ekspresslevering: + kr. 69,-</li>
+                        <li>Helger: kr. 305,-</li>
+                        <li>Levering utenfor ordinær tid: kr. 235,-</li>
+                    </ul>
+                </li>
+                <li>Brukt service og emballasje settes frem for henting.</li>
+              </ul>
+            </div>
+          </div>
+          <div class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+            <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+              <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div class="sm:col-span-2">
+                  <label for="contact_person" class="block text-sm/6 font-semibold text-gray-900">Contact person</label>
+                  <div class="mt-2.5">
+                    <input type="text" name="contact_person" id="contact_person" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="email" class="block text-sm/6 font-semibold text-gray-900">Email</label>
+                  <div class="mt-2.5">
+                    <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="phone" class="block text-sm/6 font-semibold text-gray-900">Phone number</label>
+                  <div class="mt-2.5">
+                    <input type="tel" name="phone" id="phone" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="company" class="block text-sm/6 font-semibold text-gray-900">Company</label>
+                  <div class="mt-2.5">
+                    <input type="text" name="company" id="company" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="delivery_location" class="block text-sm/6 font-semibold text-gray-900">Delivery location</label>
+                  <div class="mt-2.5">
+                    <input type="text" name="delivery_location" id="delivery_location" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="message" class="block text-sm/6 font-semibold text-gray-900">Message</label>
+                  <div class="mt-2.5">
+                    <textarea name="message" id="message" rows="4" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
+
       <div class="bg-white">
         <div class="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
 
-          <div class="rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:p-8">
+          <div class="rounded-lg bg-gray-100 px-4 py-6 sm:p-6 lg:p-8">
             <div class="flow-root">
               <dl class="-my-4 divide-y divide-gray-200 text-sm">
                 <div class="flex items-center justify-between py-4">
@@ -55,12 +123,13 @@
           </div>
       
           <div class="mt-10 flex gap-3">
-            <button type="submit" class="w-full rounded-md border border-transparent bg-red-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
+            <button class="w-full rounded-md border border-transparent bg-red-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
                 Reset form
             </button>
             <button type="submit" class="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
                 Checkout
             </button>
+          </form>
           </div>
         </div>
       </div>      
