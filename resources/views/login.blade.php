3674cf29edeb6783428d95a0219e2d6f47e8b274
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-18 23:19:34 +0200

    Correct view titles

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sillongen | Sign in</title>
+  <title>Sillongen | Logg inn</title>
   @vite('resources/css/app.css')
 </head>
 <body class="h-full font-inter">
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sillongen | Create an account</title>
+  <title>Sillongen | Opprett en konto</title>
   @vite('resources/css/app.css')
 </head>
 <body class="h-full font-inter">
