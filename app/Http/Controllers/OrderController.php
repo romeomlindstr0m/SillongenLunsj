8e944576bc2bb13f73627a2dec447e22f751a4c8
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-18 14:11:13 +0200

    Add index, show, and destroy methods to OrderController.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use App\Models\Order;
+
+use Illuminate\View\View;
 
 class OrderController extends Controller
 {
+    public function index(): View
+    {
+        $orders = Order::all();
+        return view('orders.index')->with('orders', $orders);
+    }
 
+    public function show($id): View
+    {
+        $order = Order::findOrFail($id);
+        return view('orders.show')->with('order', $order);
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        $order = Order::findOrFail($id);
+        $order->delete();
+        return back()->with('status', 'Order has been successfully deleted.');
+    }
 }
