6890973fdbe8d30e6b3f1dfa642d30a786332092
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-19 04:15:45 +0200

    Add image validation and storage in store method, implement destroy and edit methods.

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Support\Facades\Storage;
+
 class ItemsController extends Controller
 {
     public function index(): View
@@ -38,6 +40,7 @@ class ItemsController extends Controller
             'price' => ['required', 'numeric'],
             'sku' => ['required', 'max:255', 'string', 'unique:items,sku'],
             'category_id' => ['required', 'integer', 'exists:categories,id'],
+            'image' => ['file', 'mimes:jpg,png,jpeg', 'max:20480'],
         ];
 
         $messages = [
@@ -54,9 +57,13 @@ class ItemsController extends Controller
         
             'category_id.integer' => 'Kategori må være et heltall.',
             'category_id.exists' => 'Valgt kategori finnes ikke.',
+
+            'image.file' => 'Den opplastede bilden er ugyldig.',
+            'image.mimes' => 'Kun bilder av typen JPG, PNG eller JPEG er tillatt.',
+            'image.max' => 'Filen må ikke være større enn 20MB.',
         ];
 
-        $validator = Validator::make($request->only(['name', 'price', 'sku', 'category_id']), $rules, $messages);
+        $validator = Validator::make($request->only(['name', 'price', 'sku', 'category_id', 'image']), $rules, $messages);
 
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator);
@@ -69,8 +76,85 @@ class ItemsController extends Controller
         $item->sku = $request->sku;
         $item->category_id = $request->category_id;
 
+        if ($request->hasFile('image')) {
+            $image_path = $request->file('image')->store('items_images', 'public');
+            $item->image_path = $image_path;
+        }
+
         $item->save();
 
         return back()->with('status', 'En ny menyvare ble opprettet.');
+    }
+
+    public function edit($id): View
+    {
+        $item = Item::findOrFail($id);
+        $categories = Category::all();
+        return view('items.edit')->with(['item' => $item, 'categories' => $categories]);
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        $rules = [
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric'],
+            'sku' => ['required', 'max:255', 'string'],
+            'category_id' => ['required', 'integer', 'exists:categories,id'],
+            'image' => ['file', 'mimes:jpg,png,jpeg', 'max:20480'],
+        ];
+
+        $messages = [
+            'required' => 'Vennligst fyll ut alle feltene.',
+        
+            'name.string' => 'Navn må være en tekststreng.',
+            'name.max' => 'Navn kan ikke overstige 255 tegn.',
+        
+            'price.numeric' => 'Pris må være et tall.',
+        
+            'sku.string' => 'Varenummer må være en tekststreng.',
+            'sku.max' => 'Varenummer kan ikke overstige 255 tegn.',
+        
+            'category_id.integer' => 'Kategori må være et heltall.',
+            'category_id.exists' => 'Valgt kategori finnes ikke.',
+
+            'image.file' => 'Den opplastede bilden er ugyldig.',
+            'image.mimes' => 'Kun bilder av typen JPG, PNG eller JPEG er tillatt.',
+            'image.max' => 'Filen må ikke være større enn 20MB.',
+        ];
+
+        $validator = Validator::make($request->only(['name', 'price', 'sku', 'category_id', 'image']), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        }
+
+        $item = Item::find($id);
+        
+        $item->name = $request->name;
+        $item->price = $request->price;
+        $item->sku = $request->sku;
+        $item->category_id = $request->category_id;
+
+        if ($request->hasFile('image')) {
+            $image_path = $request->file('image')->store('items_images', 'public');
+            $item->image_path = $image_path;
+        }
+
+        $item->save();
+
+        return back()->with('status', 'Menyvaren ble oppdatert.');
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        $target_item = Item::findOrFail($id);
+
+        if (!empty($target_item->image_path)) {
+            Storage::disk('public')->delete($target_item->image_path);
+        }
+
+        $target_item->delete();
+
+        return back()->with('session', 'Menyvaren ble fjernet.');
     }
 }
