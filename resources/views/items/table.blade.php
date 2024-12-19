a05ba280fd7316dcb48ebcc19bc33306a8203d1c
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-19 11:56:52 +0200

    Correct translation keys in table.

--- resources/views/items/table.blade.php
+++ resources/views/items/table.blade.php
@@ -15,7 +15,7 @@
         <div class="sm:flex sm:items-center">
           <div class="sm:flex-auto">
             <h1 class="text-base font-semibold text-gray-900">Menyvarer</h1>
-            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
+            <p class="mt-2 text-sm text-gray-700">En liste over alle menyvarer, inkludert navn, pris, varenummer og kategori.</p>
           </div>
           <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
             <a href="{{ route('items.create') }}" class="block rounded-md bg-gray-900 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Legg till menyvare</a>
