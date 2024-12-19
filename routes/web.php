b9f10cbcc71523cee21439cda0f217f1844a5ce3
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-19 11:57:34 +0200

    Group dashboard routes for simplified gate authorization.

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,28 @@ Route::get('/home', function() {
     return view('home');
 })->name('home');
 
-Route::get('/dashboard', function() {
-    if (!Gate::allows('access-dashboard')) {
-        abort(403);
-    }
+Route::middleware(['auth', 'can:access-dashboard'])->group(function() {
+    Route::get('/dashboard', function() {
+        return view('dashboard');
+    })->name('dashboard');
 
-    return view('dashboard');
-})->name('dashboard')->middleware('auth');
+    Route::get('items/table', [ItemsController::class, 'table'])->name('items.table');
+    Route::get('items/create', [ItemsController::class, 'create'])->name('items.create');
+    Route::post('items/store', [ItemsController::class, 'store'])->name('items.store');
+    Route::get('items/{id}/edit', [ItemsController::class, 'edit'])->name('items.edit');
+    Route::post('items/{id}/update', [ItemsController::class, 'update'])->name('items.update');
+    Route::post('items/{id}/destroy', [ItemsController::class, 'destroy'])->name('items.destroy');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    Route::post('/users/{id}/destroy', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+
+    Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+    Route::get('/orders/{id}/show', [OrderController::class, 'show'])->name('orders.show');
+    Route::post('/orders/{id}/destroy', [OrderController::class, 'destroy'])->name('orders.destroy');
+});
 
 Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'login'])->name('login.process');
@@ -33,19 +48,5 @@ Route::post('/register', [AuthenticationController::class, 'register'])->name('r
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::get('/items', [ItemsController::class, 'index'])->name('items.index');
-Route::get('items/table', [ItemsController::class, 'table'])->name('items.table')->middleware('auth');
-Route::get('items/create', [ItemsController::class, 'create'])->name('items.create')->middleware('auth');
-Route::post('items/store', [ItemsController::class, 'store'])->name('items.store')->middleware('auth');
-Route::get('items/{id}/edit', [ItemsController::class, 'edit'])->name('items.edit')->middleware('auth');
-Route::post('items/{id}/update', [ItemsController::class, 'update'])->name('items.update')->middleware('auth');
-Route::post('items/{id}/destroy', [ItemsController::class, 'destroy'])->name('items.destroy')->middleware('auth');
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('auth');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create')->middleware('auth');
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store')->middleware('auth');
-Route::post('/users/{id}/destroy', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit')->middleware('auth');
-
-Route::get('/orders', [OrderController::class, 'index'])->name('orders.index')->middleware('auth');
-Route::get('/orders/{id}/show', [OrderController::class, 'show'])->name('orders.show')->middleware('auth');
-Route::post('/orders/{id}/destroy', [OrderController::class, 'destroy'])->name('orders.destroy')->middleware('auth');
+Route::post('/orders/store', [OrderController::class, 'store'])->name('orders.store')->middleware('auth');
